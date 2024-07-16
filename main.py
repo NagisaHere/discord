400f974dacff0b61ea2083ba1ec2d01e13d2bec4
NagisaHere <rw75000777@gmail.com>
2024-07-16 18:36:08 +1000

    added flask code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
+from discord.ext import commands
 from key import get_key
+from constants import *
+from network import start_network
 
 
 intents = discord.Intents.default()
@@ -10,6 +13,8 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
+    reboot = client.get_channel(GUILD_CHANNEL)
+    await reboot.send("I have rebooted!")
 
 @client.event
 async def on_message(message):
@@ -19,4 +24,6 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run(get_key())
+if __name__ == "__main__":
+    client.run(get_key())
+    start_network()
---
+++ network.py
@@ -0,0 +1,36 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# I have no idea what this does
+
+
+def start_network() -> None:
+    @app.route("/", methods=["POST"])
+    async def interactions():
+        raw_request = request.json
+        print(f"Request: {raw_request}")
+        return interact(raw_request)
+
+    def interact(raw_request):
+        """raw_request is in a json file"""
+        if raw_request["type"] == 1:  # ping
+            response_data = {"type": 1}
+        else:
+            # I presume the data would just be whatever is passed by the slash
+            # command
+            data = raw_request["data"]
+            command_name = data["name"]
+
+            if command_name == "echo":
+                original_message = data["options"][0]["value"]
+                message_content = f"Echoing {original_message}"
+
+            response_data = {
+                "type": 4,
+                "data": {"content": message_content}
+            }
+
+        return jsonify(response_data)
+
+app.run(debug=True)
